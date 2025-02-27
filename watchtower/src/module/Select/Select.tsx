5882aa1bc1cd0c4bf16c5b108b82130e77422e26
sinaafshar13 <afshari.sina@outlook.com>
2025-02-27 15:26:53 +0330

    [ADD] styles of select.module.css [UPDATE] color of index.css

--- watchtower/src/module/Select/Select.tsx
+++ watchtower/src/module/Select/Select.tsx
@@ -1,6 +1,7 @@
 import { ComponentProps, ReactNode } from "react";
 
 import styles from "./Select.module.css";
+import { SelectOption } from "../../types/select-option";
 
 type Props = ComponentProps<"select"> & {
   options: SelectOption[];
@@ -8,9 +9,15 @@ type Props = ComponentProps<"select"> & {
 
 const Select = ({ options }: Props): ReactNode => {
   return (
-    <select className={styles.select} name="category" id="category">
-      {options.map((option) => <option key={option.value} value={option.value}>{option.label}</option>)}
-    </select>
+    <div className={styles.select}>
+      <select name="category" id="category">
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
