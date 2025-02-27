f4847866882d3cf6196a06cd6c09a0e02f69fa13
sinaafshar13 <afshari.sina@outlook.com>
2025-02-27 09:19:05 +0330

    [ADD] raw html and css

--- watchtower/src/App.tsx
+++ watchtower/src/App.tsx
@@ -1,12 +1,40 @@
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
 
-import './App.css'
+import "./App.css";
+import { Media } from "./types/media";
 
 function App(): ReactNode {
-
+  const media: Media[] = [
+    {
+      id: "1",
+      title: "A million dollar baby",
+      description: "this is about a girl who wants to be a boxer ...",
+      date: new Date(2005, 1, 14),
+      category: "movie",
+    },
+  ];
   return (
-   <h1>watchtower</h1>
-  )
+    <>
+      <header>Watchtower</header>
+      <main>
+        <div className="toolbar">
+          <input type="text" className="search" />
+          <select name="category" id="category">
+            <option value="all">All</option>
+            <option value="movie">Movie</option>
+            <option value="series">Series</option>
+            <option value="book">Book</option>
+          </select>
+          <button></button>
+        </div>
+        <ul className="result"> 
+          {media.map((item) => (
+            <li>{item.title}</li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ watchtower/src/types/media.ts
@@ -0,0 +1,9 @@
+import { Category } from "./category"
+
+export type Media = {
+    id : "string",
+    title : "string",
+    description : "string",
+    date: "string",
+    category: Category
+}
