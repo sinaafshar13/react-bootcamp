1c22171cc02e548004869df0c8d5ea3b2646a9ff
sinaafshar13 <afshari.sina@outlook.com>
2025-03-01 08:04:49 +0330

    [FIX] styles of Create component [UPDATE] index.css and button.css

--- watchtower/src/components/Create/Create.tsx
+++ watchtower/src/components/Create/Create.tsx
@@ -2,11 +2,14 @@ import { ReactNode } from "react";
 
 import styles from "./Create.module.css";
 import Button from "../../module/Button/Button";
+import MingcuteAddSquareLine from "../../icons/MingcuteAddSquareLine";
 
 const Create = (): ReactNode => {
   return (
     <div className={styles.create}>
-      <Button variant="solid" shape="circle"></Button>
+      <Button variant="solid" shape="circle">
+        <MingcuteAddSquareLine />
+      </Button>
     </div>
   );
 };
